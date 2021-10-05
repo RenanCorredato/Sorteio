d456284ef58227a1bca18d5d6574e4d9c599c32f
Renan Corredato <renancorredato@hotmail.com>
2021-10-05 13:38:02 -0300

    criando metodo gera numero aleatorio

--- app/src/main/java/com/renancorredato/MainActivity.kt
+++ app/src/main/java/com/renancorredato/MainActivity.kt
@@ -3,17 +3,28 @@ package com.renancorredato
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.renancorredato.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btPlay.setOnClickListener {
+            drawNumber()
+        }
 
 
     }
 
-    fun drawNumber(view: View){
-
-
+    fun drawNumber(){
+        val textChoose = binding.tvSelectingNumber
+        val drawNumberRandom = Random().nextInt(11)
+        textChoose.text = "Número sorteado é : $drawNumberRandom"
     }
 }
